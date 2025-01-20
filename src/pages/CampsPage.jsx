6e05f25371634a00500075711b1f9230342f2e08
Michael P <102651473+michmich16@users.noreply.github.com>
2025-01-20 13:57:17 +0100

    done for now

--- src/pages/CampsPage.jsx
+++ src/pages/CampsPage.jsx
@@ -1,11 +1,37 @@
 import { Hero } from '../components/Hero/Hero';
-import {SectionTitle} from '../components/SectionTitle/SectionTitle';
+import { GridContainer } from '../components/GridContainer/GridContainer';
+import { NewsCard } from '../components/NewsCard/NewsCard';
+import { SectionTitle } from '../components/SectionTitle/SectionTitle';
+import { useGet } from '../hooks/useGet';
+import { MarginContainer } from '../components/MarginContainer/MarginContainer';
+import { NavLink } from 'react-router-dom';
 
 export const CampsPage = () => {
+
+    const { data, isLoading, error } = useGet(
+        `https://api.mediehuset.net/mediesuset/camps`
+    );
+
     return (
         <>
-            <Hero img="crowd3-foto-colourbox.jpg" imgPosition={'100% 80%'}/>
-            <SectionTitle title={"CAPMS"} />
+                        <Hero img="crowd3-foto-colourbox.jpg" imgPosition={'100% 80%'}/>
+                        <SectionTitle title={"CAPMS"} />
+            <MarginContainer>
+                <GridContainer columns={3} gap={"3rem"}>
+                    {!isLoading && data?.items
+                        .map((item) => {
+                            return (
+                                <NewsCard
+                                    key={item.id}
+                                    img={item.image}
+                                    title={item.name}
+                                    text={item.description}
+                                />
+                            );
+                        })}
+                </GridContainer>
+            </MarginContainer>
+
         </>
     );
 };
--- src/pages/InfoPage.jsx
+++ src/pages/InfoPage.jsx
@@ -1,11 +1,31 @@
 import { Hero } from '../components/Hero/Hero';
 import { SectionTitle } from '../components/SectionTitle/SectionTitle';
+import { MarginContainer } from '../components/MarginContainer/MarginContainer';
+import { GridContainer } from '../components/GridContainer/GridContainer';
+import map from '../assets/images/mediesuset-map.jpg';
+import style from '../styles/infoPage.module.scss';
 
 export const InfoPage = () => {
     return (
         <>
-            <Hero img="crowd3-foto-colourbox.jpg" imgPosition={'100% 80%'}/>
-            <SectionTitle title={"PRAKTISK INFO"}/>
+            <Hero img="crowd3-foto-colourbox.jpg" imgPosition={'100% 80%'} />
+            <SectionTitle title={"PRAKTISK INFO"} />
+            <MarginContainer>
+                <GridContainer columns={2}>
+                    <div className={style.infoText}>
+                        <p>
+                            Adresse: Øster Uttrupvej 1
+                        </p>
+                        <p>
+                            Aalborg 9000
+                        </p>
+                    </div>
+                    <div className={style.infoImg}>
+                    <img src={map} alt="map" />
+                    </div>
+                </GridContainer>
+            </MarginContainer>
+
         </>
     );
 };
